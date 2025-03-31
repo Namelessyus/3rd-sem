c3d354c415da19d672adf100727482bd85dcabf0
namelessschool <anushmasky@gmail.com>
2025-03-31 08:12:03 +0545

    web

--- DSA/queue.c
+++ DSA/queue.c
@@ -10,59 +10,57 @@ int main(){
 	rear=-1;
 	front=-1;
 	
-printf("Enter lenght of queue (n<=100):");
-scanf("%d", &n);
-
-printf("\n 1 for enqueue \n 2 for dequeue \n 3 for display \n 4 for exit");
-while(1){
-printf("\n Enter your choice: ");
-scanf("%d" ,&choice);
-switch(choice){
-case 1:
-	enqueue();
-break;
-case 2:
-	dequeue();
-break;
-case 3:
-	display();
-break;
-case 4:
-	exit(0);
-		default:
-			{
-				printf("\n Enter a number between 1-5");
-			}
+	printf("Enter lenght of queue (n<=100):");
+	scanf("%d", &n);
+	
+	printf("\n 1 for enqueue \n 2 for dequeue \n 3 for display \n 4 for exit");
+	while(1){
+		printf("\n Enter your choice: ");
+		scanf("%d" ,&choice);
+		switch(choice){
+			case 1:
+				enqueue();
+			break;
+			case 2:
+				dequeue();
+			break;
+			case 3:
+				display();
+			break;
+			case 4:
+				exit(0);
+			default:
+				printf("\n Enter a number between 1-5");		
 		}
-	}	
-	return 0;
 	}
+	return 0;
+}
 	
 void enqueue(){
-if (rear >= n-1){
-printf ("\n Queue is overflowing");
-}else {
-rear=rear+1;
-printf("\n Enter the value to be pushed: ");
-scanf("%d", &x);
-if(front==-1){
-	front=0;
-}
-queue[rear]=x;
-}
+	if (rear >= n-1){
+		printf ("\n Queue is overflowing");
+	}else{
+		rear=rear+1;
+		printf("\n Enter the value to be pushed: ");
+		scanf("%d", &x);
+		if(front==-1){
+			front=0;
+		}
+	queue[rear]=x;
+	}
 }
 
 
 void dequeue(){
-if(front == -1 || front==rear+1)
-{
-printf("\n Queue is underflow");                                                                                                                                                                          
-}
-else
-{
-printf("\n The dequeue element is %d", queue[front]);
-front++;
-}
+	if(front == -1 || front==rear+1)
+	{	
+		printf("\n Queue is underflow");                                                                                                                                                                          
+	}
+	else
+	{
+		printf("\n The dequeue element is %d", queue[front]);
+		front++;
+	}
 }
 
 
@@ -71,19 +69,13 @@ void display()
 if(rear>=0)
 {
 printf("\n The element in Queue");
-for(i=rear; i>=front; i--){
-printf("\n %d", queue[i]);
+for(i=rear; i>=front; i--){ 
+	printf("\n %d", queue[i]);
 }
 
 }
 else{
-printf("\n The queue is empty");
+	printf("\n The queue is empty");
 }
 }
-
-
-
-
-
-
 
