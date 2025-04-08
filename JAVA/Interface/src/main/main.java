675ba48991b85fda4a0db824ccf89d0f11b4fa21
Namelessyus <anushmasky@gmail.com>
2025-04-08 10:13:20 +0545

    java

--- JAVA/Interface/src/main/main.java
+++ JAVA/Interface/src/main/main.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
 /**
- *
- * @author anush
+Question 1: Create a Java program for a where different payment methods like CreditCard, PayPal, 
+and BankTransfer implement a PaymentMethod interface. The interface should define a method processPayment(double amount). 
+Write a main class that demonstrates processing a payment using all three methods.
+Demonstrate how interfaces allow different classes to follow a common contract and support polymorphism.
  */
+
 public class main {
     public static void main(String args[]){
         BankTransfer bank = new BankTransfer();
